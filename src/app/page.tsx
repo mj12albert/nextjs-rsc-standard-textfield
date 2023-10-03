ef5f831e5276968465f8671a07ef5e6a1c82a480
CodeSandbox <support@codesandbox.io>
2023-10-03 09:08:40 +0000

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 import * as React from "react";
-import { Typography } from "@mui/material";
+import { Typography, TextField } from "@mui/material";
 
 export default function HomePage() {
-  return (
-    <Typography>
-      MUI example. Please put the code to reproduce the issue in src/App.tsx
-    </Typography>
-  );
+  return <TextField id="standard-basic" label="Standard" variant="standard" />;
 }
